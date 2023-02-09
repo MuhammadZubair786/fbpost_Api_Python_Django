441651032d474531ffd258499150e0ee3be30369
Muhammad Zubair <57016162+MuhammadZubair786@users.noreply.github.com>
2023-02-09 17:59:04 +0500

    Update models.py

--- home/models.py
+++ home/models.py
@@ -1,6 +1,5 @@
 import json
 from django.db import models
-from froala_editor.fields import FroalaField
 from django.contrib.auth.models import User
 from .helpers import *
 from .helpers import generate_slug
@@ -8,7 +7,7 @@ from .helpers import generate_slug
 
 class BlogModel(models.Model):
     title = models.CharField(max_length=1000)
-    content = FroalaField()
+    content = models.CharField(max_length=1000)
     slug = models.SlugField(max_length=1000, null=True, blank=True)
     image = models.ImageField(upload_to='blog')
     created_at = models.DateTimeField(auto_now_add=True)
