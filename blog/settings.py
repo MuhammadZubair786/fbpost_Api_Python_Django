ed54322714f8195be43479c3d53982833cf8a016
Muhammad Zubair <57016162+MuhammadZubair786@users.noreply.github.com>
2023-02-09 16:19:43 +0500

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ctb=f@d63bttmtc3(zmg@!505po%o%ymzsull7ej^+)s)3us+e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
